13dd08b87195a0772f7ed12362f9ac2f15c1e323
OdedaraBhavesh <imbpodedara987@gmail.com>
2026-06-16 09:04:50 +0530

    Fix: Editor and Manager Roles

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import Bookmark, Category, Blog, Comment, Follow, Reaction, Tag, UserProfile
+from django.contrib.auth.models import Group
+
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'category', 'author', 'status', 'is_featured')
     search_fields = ('id', 'title', 'category__category_name', 'status')
     list_editable = ('is_featured',)
+
 
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
@@ -15,3 +18,4 @@ admin.site.register(UserProfile)
 admin.site.register(Follow)
 admin.site.register(Reaction)
 admin.site.register(Bookmark)
+admin.site.unregister(Group)
